131c12aadd053af6026080664c25894df7eda220
aidanywu <aidanywu@gmail.com>
2021-11-16 15:07:44 -0800

    Created Repository

---
+++ main.py
@@ -0,0 +1,17 @@
+# import "packages" from flask
+from flask import Flask, render_template
+
+# create a Flask instance
+app = Flask(__name__)
+
+
+# connects default URL to render index.html
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+# runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
