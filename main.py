bf27010552dfa5d81b049037169aa8de407aef9f
3ghin5 <yoylecakeisback@gmail.com>
2021-11-18 14:53:04 -0800

    creating test pages to figure out why the website isnt doing anythign

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ def aboutus():
     return render_template("aboutus.html")
 
 
+@app.route("/williamli/")
+def williamli():
+    return render_template("williamli.html")
+
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
