fe8045beb833f4fc4af18e820d20d1532a3fd665
aidanywu <aidanywu@gmail.com>
2021-12-10 15:13:18 -0800

    new dictionary api nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -25,8 +25,9 @@ def williamli():
     return render_template("about/williamli.html")
 
 
-@app.route("/aidanwu/")
+@app.route("/aidanwu/", methods=['GET','POST'])
 def aidanwu():
+    # time api
     url = "https://world-clock.p.rapidapi.com/json/pst/now"
     headers = {
         'x-rapidapi-host': "world-clock.p.rapidapi.com",
@@ -34,7 +35,20 @@ def aidanwu():
     }
     response = requests.request("GET", url, headers=headers)
     time = json.loads(response.text)
-    return render_template("about/aidanwu.html", query=time)
+    # dictionary api
+    url1 = "https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary"
+    querystring1 = {"word":"word"}
+    if request.form:
+        word = request.form.get("word")
+        if len(word) != 0:  # input field has content
+            querystring1["word"] = word
+    headers1 = {
+        'x-rapidapi-host': "dictionary-by-api-ninjas.p.rapidapi.com",
+        'x-rapidapi-key': "1d9c0e5dd4msh00cea2fa8d7699fp1dfecdjsn1cf8da6644a9"
+    }
+    response1 = requests.request("GET", url1, headers=headers1, params=querystring1)
+    data = json.loads(response1.text)
+    return render_template("about/aidanwu.html", query=time, data=data, word=querystring1["word"])
 
 
 @app.route("/williamwu/")
