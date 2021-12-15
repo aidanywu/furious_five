40e95aa720560589d27eb8d341d5823df2f21eca
vaishavijay <85912486+vaishavijay@users.noreply.github.com>
2021-12-14 18:08:17 -0800

    finished notebooks?!

--- main.py
+++ main.py
@@ -81,6 +81,20 @@ def cafe():
     return render_template("studyrooms/cafe.html")
 
 
+@app.route("/notebook1/")
+def notebook1():
+    return render_template("notebooks/notebook1.html")
+
+
+@app.route("/notebook2/")
+def notebook2():
+    return render_template("notebooks/notebook2.html")
+
+
+@app.route("/notebook3/")
+def notebook3():
+    return render_template("notebooks/notebook3.html")
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
